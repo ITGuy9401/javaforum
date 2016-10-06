0c5b9308873983a0fd0132f0f0ee2cd55c1c86c4
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2016-10-06 13:59:04 +0200

    sviluppo architettura DAO

--- core/src/main/java/eu/arcangelovicedomini/javaforum/core/domain/ForumDao.java
+++ core/src/main/java/eu/arcangelovicedomini/javaforum/core/domain/ForumDao.java
@@ -2,8 +2,7 @@ package eu.arcangelovicedomini.javaforum.core.domain;
 
 import eu.arcangelovicedomini.javaforum.core.vo.ForumVO;
 
-public class ForumDao implements Dao<Forum, ForumVO, Long> {
+public class ForumDao extends DaoBase<Forum, ForumVO, Long> {
 
-	
 
 }
