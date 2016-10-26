ce03a1f7c9ec82648ff7311fc4285d4b2d19e5cb
itguy9401 <io@arangelovicedomini.eu>
2016-10-26 21:06:59 +0200

    development

--- core/src/main/java/eu/arcangelovicedomini/javaforum/core/domain/ForumDao.java
+++ core/src/main/java/eu/arcangelovicedomini/javaforum/core/domain/ForumDao.java
@@ -1,7 +1,10 @@
 package eu.arcangelovicedomini.javaforum.core.domain;
 
+import org.springframework.stereotype.Repository;
+
 import eu.arcangelovicedomini.javaforum.core.vo.ForumVO;
 
+@Repository
 public class ForumDao extends DaoBase<Forum, ForumVO, Long> {
 
 
