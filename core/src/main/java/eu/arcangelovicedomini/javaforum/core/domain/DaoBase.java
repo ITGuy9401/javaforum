89ead19cbb291c5b6a3f8e4de0c5dc4c55897747
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2016-11-09 13:49:25 +0100

    development of dao architecture

--- core/src/main/java/eu/arcangelovicedomini/javaforum/core/domain/DaoBase.java
+++ core/src/main/java/eu/arcangelovicedomini/javaforum/core/domain/DaoBase.java
@@ -1,12 +1,25 @@
 package eu.arcangelovicedomini.javaforum.core.domain;
 
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
 public abstract class DaoBase<En, Vo, Id extends Serializable> extends HibernateDaoSupport implements Dao<En, Vo, Id> {
 	
+	private Class<En> persistentClass;
+	private Class<Vo> voClass;
+
+	@SuppressWarnings("unchecked")
+	public DaoBase() {
+	    this.persistentClass = (Class<En>) ((ParameterizedType) getClass()
+	                            .getGenericSuperclass()).getActualTypeArguments()[0];
+	    this.voClass = (Class<Vo>) ((ParameterizedType) getClass()
+                .getGenericSuperclass()).getActualTypeArguments()[1];
+	 }
 	
 	@Override
 	public En create(En entity) {
@@ -26,9 +39,11 @@ public abstract class DaoBase<En, Vo, Id extends Serializable> extends Hibernate
 		getHibernateTemplate().delete(entity);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<En> findAll() {
-		getHibernateTemplate().loadAll(En); //FIXME
+		List<En> entities = (List<En>) getHibernateTemplate().loadAll(persistentClass.getClass());
+		return entities;
 	}
 
 	@Override
@@ -37,19 +52,37 @@ public abstract class DaoBase<En, Vo, Id extends Serializable> extends Hibernate
 		getHibernateTemplate().deleteAll(findAll());
 	}
 	
+	@SuppressWarnings("unchecked")
+	@Override
 	public En get(Id id) {
-		getHibernateTemplate().load(En, id); // FIXME
+		En entity = (En) getHibernateTemplate().load(persistentClass.getClass(), id);
+		return entity;
 	}
 	
+	protected abstract void toVo(En entity, Vo vo);
+	
+	@Override
 	public Vo toVo(En entity) {
-		return null;
+		try {
+			Vo newInstance = voClass.newInstance();
+			toVo(entity, newInstance);
+			return newInstance;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
+	@Override
 	public List<Vo> toVoCollection(List<En> entities) {
-		return null;
+		List<Vo> vos = new ArrayList<>();
+		for (En en : entities) {
+			vos.add(toVo(en));
+		}
+		return vos;
 	}
 	
+	@Override
 	public List<Vo> toVoCollection(En... entities) {
-		return null;
+		return toVoCollection(Arrays.asList(entities));
 	}
 }
