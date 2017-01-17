e3f1f4a035d99f15894b2b03a52db24505fb4baa
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-01-18 00:58:56 +0100

    sviluppo

--- core/src/main/java/eu/arcangelovicedomini/javaforum/core/exception/JFException.java
+++ core/src/main/java/eu/arcangelovicedomini/javaforum/core/exception/JFException.java
@@ -1,6 +1,11 @@
 package eu.arcangelovicedomini.javaforum.core.exception;
 
 public class JFException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public static enum ExceptionType {
 		TECHNICAL, BUSINESS
 	}
@@ -13,6 +18,38 @@ public class JFException extends Exception {
 		this.message = message;
 		this.messageKey = messageKey;
 		this.messageArgs = messageArgs;
+		this.exceptionType = exceptionType;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessageKey() {
+		return messageKey;
+	}
+
+	public void setMessageKey(String messageKey) {
+		this.messageKey = messageKey;
+	}
+
+	public Object[] getMessageArgs() {
+		return messageArgs;
+	}
+
+	public void setMessageArgs(Object[] messageArgs) {
+		this.messageArgs = messageArgs;
+	}
+
+	public ExceptionType getExceptionType() {
+		return exceptionType;
+	}
+
+	public void setExceptionType(ExceptionType exceptionType) {
 		this.exceptionType = exceptionType;
 	}
 }
