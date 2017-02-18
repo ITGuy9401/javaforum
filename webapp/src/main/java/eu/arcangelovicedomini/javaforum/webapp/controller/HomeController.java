edbf138d51804186d3800892bb6a293bf76f00ed
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-02-18 13:41:33 +0100

    development

--- webapp/src/main/java/eu/arcangelovicedomini/javaforum/webapp/controller/HomeController.java
+++ webapp/src/main/java/eu/arcangelovicedomini/javaforum/webapp/controller/HomeController.java
@@ -2,7 +2,12 @@ package eu.arcangelovicedomini.javaforum.webapp.controller;
 
 import org.springframework.stereotype.Controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller("homeController")
 public class HomeController {
-
+    public Map<String, Object> getForumData(){
+        Map<String, Object> result = new HashMap<>();
+    }
 }
