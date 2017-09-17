4331124f236b4a25d4d65697e03ce9b811940ec2
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-09-17 18:19:42 +0200

    development

--- api/src/main/java/eu/arcangelovicedomini/javaforum/api/domain/Area.java
+++ api/src/main/java/eu/arcangelovicedomini/javaforum/api/domain/Area.java
@@ -1,4 +1,16 @@
 package eu.arcangelovicedomini.javaforum.api.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "AREA")
 public class Area {
+    private String uuid;
+    private Long order;
+    private Forum forum;
+    private String name;
+    private String description;
+    private List<Section> sections;
 }
