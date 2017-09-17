50d2086284f547805b69a43375ef5d27b221e924
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-09-17 18:21:54 +0200

    development

--- api/src/main/java/eu/arcangelovicedomini/javaforum/api/domain/Area.java
+++ api/src/main/java/eu/arcangelovicedomini/javaforum/api/domain/Area.java
@@ -1,7 +1,8 @@
 package eu.arcangelovicedomini.javaforum.api.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -13,4 +14,62 @@ public class Area {
     private String name;
     private String description;
     private List<Section> sections;
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "UUID")
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    @Column(name = "ORDER")
+    public Long getOrder() {
+        return order;
+    }
+
+    public void setOrder(Long order) {
+        this.order = order;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "FORUM_ID")
+    public Forum getForum() {
+        return forum;
+    }
+
+    public void setForum(Forum forum) {
+        this.forum = forum;
+    }
+
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @OneToMany(mappedBy = "AREA_ID")
+    public List<Section> getSections() {
+        return sections;
+    }
+
+    public void setSections(List<Section> sections) {
+        this.sections = sections;
+    }
 }
